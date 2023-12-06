be5de931b117b4a10b985aaccb03696de5f0b2b2
Fernando Ramos <fernando.ramos9@upr.edu>
2023-12-06 11:31:43 -0400

    Initial pipeline structure

---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+from dask import delayed
+@delayed
+def get_image():
+    pass
+@delayed
+def open_image():
+    pass
+@delayed
+def calculate_AFAI():
+    pass
+
+def get_mask():
+    pass
+@delayed
+def no_observation_class():
+    pass
+@delayed
+def result():
+    pass
+
+def main(argv):
+    pass
+if __name__ == '__main__':
+    main(sys.argv[1])
